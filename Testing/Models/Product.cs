d73eeadc2390178f95a979b21dd82368de4eedbc
Rubi Meneses <rubi.ventura@icloud.com>
2022-09-19 13:55:37 -0500

    added ability to create a new product

--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Testing.Models
 {
     public class Product
@@ -12,6 +14,7 @@ namespace Testing.Models
         public int CategoryID { get; set; }
         public int OnSale { get; set; }
         public int StockLevel { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
 
